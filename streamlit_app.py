8750bd8927e10b3b771b3187b97ec22f600da930
willtollan <137554001+willtollan@users.noreply.github.com>
2026-05-26 18:55:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 RFM Customer Segmentation')
 
 st.info('This app builds a machine learning model')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
