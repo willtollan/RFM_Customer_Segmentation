61b634fc6faf95fd0cbe7f272c173ba39139ac4b
willtollan <137554001+willtollan@users.noreply.github.com>
2026-06-27 16:58:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -208,7 +208,7 @@ with st.expander('Random Forest Classifier', expanded=False):
     st.subheader('Confusion Matrix')
     st.write('Matrix visualising the actual versus predicted classification distributions on test data subsets:')
     
-    cm_col1, cm_col2, cm_col3 = st.columns([1, 6, 1])
+    cm_col1, cm_col2, cm_col3 = st.columns([1, 4, 1])
     with cm_col2:
         st.image('images/tuned_RF_confusion_matrix.png', use_column_width=True)
 
