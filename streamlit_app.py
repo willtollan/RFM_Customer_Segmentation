2c7baf226054195c659b9bc69d1c8df8cecb26ba
willtollan <137554001+willtollan@users.noreply.github.com>
2026-06-27 17:47:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,7 @@ with st.expander('KMeans Clustering Results and Visualisations', expanded=False)
     v_col1, v_col2, v_col3 = st.columns([1.5, 5, 1.5])
     with v_col2:
         try:
-            st.image('images/cluster_boxplot_by_features.png', width=800)
+            st.image('images/cluster_boxplot_by_features.png', width=900)
         except FileNotFoundError:
             st.error("Could not find 'images/cluster_boxplot_by_features.png'.")
 
